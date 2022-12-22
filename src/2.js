7551c50c8d6c42709b63fe27de9df95d00e70eb8
ignat <buyfng@gmail.com>
2022-12-22 16:25:27 +0100

    switch to commonjs modules

--- src/2.js
+++ src/2.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { csv } from 'csvtojson';
+import csvtojson from 'csvtojson';
 import { pipeline } from 'node:stream';
 
 const inFilePath = process.argv[2]
@@ -7,7 +7,7 @@ const outFilePath = process.argv[3]
 
 pipeline(
   fs.createReadStream(inFilePath),
-  csv(),
+  csvtojson.csv(),
   fs.createWriteStream(outFilePath),
   (error) => {
     if (error) {
