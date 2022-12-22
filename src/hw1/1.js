55f193f02494f70cbd9f0fc207c13f7f7b2ecd6e
ignat <buyfng@gmail.com>
2022-12-22 16:35:45 +0100

    add eslint

--- src/hw1/1.js
+++ src/hw1/1.js
@@ -3,9 +3,9 @@ import * as readline from 'node:readline';
 const reverse = str => [...str].reverse().join('');
 
 const rl = readline.createInterface({
-  input: process.stdin
+    input: process.stdin
 });
 
 rl.on('line', (line) => {
-  console.log(reverse(line));
+    console.log(reverse(line));
 });
---
+++ src/hw1/2.js
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import csvtojson from 'csvtojson';
+import { pipeline } from 'node:stream';
+
+const inFilePath = process.argv[2];
+const outFilePath = process.argv[3];
+
+pipeline(
+    fs.createReadStream(inFilePath),
+    csvtojson.csv(),
+    fs.createWriteStream(outFilePath),
+    (error) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('finished');
+        }
+    }
+);
